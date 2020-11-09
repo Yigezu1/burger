3c91305e6ff5a0cf67b4384497e99d879b14b58b
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-11-08 23:00:17 -0500

    added margine to the html elements in the index.handlebar

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Mexican Avocado Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mexican Avocado Burger", false);
 
 INSERT INTO burgers (burger_name, devoured) VALUES ("The Ramly Burger", false);
 
-INSERT INTO burgers (burger_name, devoured) VALUES ("Spiced-Lamb Burgers", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Spiced-Lamb Burgers", false);
