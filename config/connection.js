4e2a3f247c5d4cb4b4990202828048d70f8977c6
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-11-08 22:11:58 -0500

    added config file

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var connection;
 if(oricess.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
-connection= mysql.createConnection({
+  connection= mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
