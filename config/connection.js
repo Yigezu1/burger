3269b32ade4d7eb201e21835d739a33d5697a9ef
Yigezu Mulugeta <Yigezumb@gmail.com>
2020-11-08 22:19:16 -0500

    updated the connection file

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 
 var connection;
-if(oricess.env.JAWSDB_URL){
+if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else{
   connection= mysql.createConnection({
